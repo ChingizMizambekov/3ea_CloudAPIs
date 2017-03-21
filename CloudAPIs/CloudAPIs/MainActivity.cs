eadef96c87f9420eebdb8be87f975a52da277869
Chingiz Mizambekov <chinjka_m@hotmail.com>
2017-03-21 11:28:17 +0100

    Basic listview implemented

---
+++ CloudAPIs/CloudAPIs/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace CloudAPIs
+{
+    [Activity(Label = "CloudAPIs", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        // Create strings list
+        private List<string> listItems;
+
+        // Reference to the XAML listview
+        private ListView myListView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Ref to the XAML ListView
+            myListView = FindViewById<ListView>(Resource.Id.MyListView);
+
+            // Populate the list
+            listItems = new List<string>();
+            listItems.Add("Bobby");
+            listItems.Add("Tommy");
+            listItems.Add("Jimmy");
+
+            // Create adapter
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, listItems);
+
+            myListView.Adapter = adapter;
+           
+
+        }
+    }
+}
+
